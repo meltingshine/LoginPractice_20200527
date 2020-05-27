75c3f31518afb6d6ed7d7f4e17862f8f23b653cf
meltingshine <meltingshine@gmail.com>
2020-05-27 20:00:41 +0900

    버튼 누르면 => 입력된 내용으로 문구 변경

--- app/src/main/java/com/example/loginpractice_20200527/MainActivity.kt
+++ app/src/main/java/com/example/loginpractice_20200527/MainActivity.kt
@@ -17,6 +17,10 @@ class MainActivity : AppCompatActivity() {
             Log.d("이벤트확인", "버튼 클릭")
             Log.e("이벤트확인","에러 로그 발생")
             Toast.makeText(this,"버튼 클릭 됨",Toast.LENGTH_LONG).show()
+
+            val inputContent = contentEdt.text.toString()
+
+            eventBtn.text = inputContent
         }
     }
 }
