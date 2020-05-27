8b816ccdd5af1b63f5f9cfd3351824b62d508f26
meltingshine <meltingshine@gmail.com>
2020-05-27 20:52:49 +0900

    로그인 체크까지

--- app/src/main/java/com/example/loginpractice_20200527/MainActivity.kt
+++ app/src/main/java/com/example/loginpractice_20200527/MainActivity.kt
@@ -12,6 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loginBtn.setOnClickListener {
+            val inputEmail = emailEdt.text.toString()
+            val inputPw =   pwEdt.text.toString()
+
+            // email / 비번 확인 해서 맞으며 / 틀리면 처리가 다름.
+            // => 분기 처리 => 조건문
+
+            if (inputEmail == "admin@test.com" && inputPw == "qwer") {
+                Log.d("로그인확인","관리자맞다")
+                Toast.makeText(this, "관리자입니다",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Log.d("로그인확인","관리자아님")
+                Toast.makeText(this, "관리자입니다",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
         eventBtn.setOnClickListener {
 
             Log.d("이벤트확인", "버튼 클릭")
